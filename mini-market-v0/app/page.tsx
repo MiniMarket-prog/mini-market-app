7e1c4e42d6fae9c5db0d7871324cc889d45df4f7
MiniMarket-prog <newtitle2010@gmail.com>
2025-02-14 15:01:15 +0000

    update

--- mini-market-v0/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Welcome to Mini Market Management</h1>
-      <p>This is the main page of your mini market management application.</p>
-    </div>
-  )
-}
-
